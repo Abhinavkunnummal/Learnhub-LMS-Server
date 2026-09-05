c1569737cae2c11654353af61029aaf7c579952b
AbhinavK <abhinavkkunnummal@gmail.com>
2026-09-05 17:25:10 +0530

    Learnhub-LMS-Server

--- index.js
+++ index.js
@@ -23,7 +23,15 @@ require("./models/MeetSession");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://lms-client.vercel.app"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const path = require("path");
